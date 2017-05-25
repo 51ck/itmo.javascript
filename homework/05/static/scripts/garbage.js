fb53e66cc483dba0bc9756a44bab9ad1f4997452
51ck <sic.popper@gmail.com>
2017-05-25 10:26:50 +0300

    homework #5

---
+++ homework/05/static/scripts/garbage.js
@@ -0,0 +1,140 @@
+(function(app){
+    let quickSort = function(array, start = 0, end = array.length - 1) {
+
+        var getPivot = function(array, start, end) {
+            var pivot = Math.floor( ( array[start] + array[end] ) / 2 );
+            return pivot;
+        };
+
+        var part = function( array, start, end ) {
+            var pivot = getPivot(array, start, end)
+                , i = start
+                , j = end
+                ;
+            while ( i <= j ) {
+                while ( array[i] < pivot) {
+                    i++;
+                };
+                while ( array[j] > pivot) {
+                    j--;
+                };
+                if ( i <= j ) {
+                    [ array[i], array[j] ] = [ array[j], array[i] ];
+                    i++;
+                    j--;
+                };
+            };
+            return i;
+        };
+
+        var qsort = function( array, start, end ) {
+            var index;
+            if ( array.length > 1 ) {
+                index = part( array, start, end );
+                if ( start < index - 1 ) {
+                    qsort( array, start, index - 1 );
+                };
+                if ( end > index ) {
+                    qsort( array, index, end );
+                };
+            };
+        return array;
+        };
+
+            return qsort( array, start, end );
+    };
+
+    let objectMerge = function( A, B = {}, recursive = true ) {
+        if (!A) { return B };
+        if (!B) { return A };
+
+        if ( typeof A === 'object' ) {
+            for (var key in B) {
+                if ( recursive && typeof A[key] === 'object' && typeof B[key] === 'object' ) {
+                   A[key] = objectMerge( A[key], B[key], true );
+                } else {
+                    A[key] = B[key];
+                };
+            };
+            return A;
+        };
+    };
+
+    let stringToIntArray = function( string ) {
+        if ( typeof string === 'string' ) {
+            if ( /\d/.test(string) ) {
+                var arr = string.match( /-?\d+/g );
+                return arr.map(function(n) { return parseInt(n); });
+            } else {
+                return [];
+            };
+        } else {
+            return null;
+        };
+    };
+
+    let universalCounter = function() {
+        var dictionary = {};
+        return function( item ) {
+            dictionary[ item ] = dictionary[ item ] === undefined ? 1 : dictionary[ item ] + 1;
+            return dictionary[ item ];
+        };
+    };
+
+    let weedOut = function( array, filter = function( item, index, array) { return true; }) {
+        if ( array instanceof Array ) {
+            var result = [];
+            for ( var i = 0, n = array.length; i < n; i++ ) {
+                if ( filter( array[i], i, array ) ) {
+                    result.push( array[i] );
+                };
+            };
+        } else {
+            return array;
+        };
+    };
+
+    let bump = function( number, char = ' ', length = number.toString().length ) {
+        var result = number.toString();
+        var filler = '';
+
+        if ( result.length < length ) {
+            var fillLength = length - result.length;
+            if ( char.length > 1 ) {
+                while ( fillLength > 0 ) {
+                    var i = 0,
+                        n = char.length;
+                    while ( i < n && fillLength-- ) {
+                        filler += char[i++];
+                    };
+                };
+            } else {
+                filler = Array( fillLength + 1 ).join( char );
+            };
+        };
+        return filler + result;
+    };
+
+    let printObject = function( object, recursive = true, level = 1 ) {
+        var result = '';
+        for ( var key in object ) {
+            if ( recursive && typeof object[key] === 'object' ) {
+                result += `${Array( level ).join( '\t' ) }${ key }:\n` + printObject( object[key], true, level + 1 );
+            } else {
+                result += `${Array( level ).join( '\t' ) }${ key }: ${ object[key] }\n`;
+            };
+        };
+        return result;
+    };
+
+    app.tools = {
+        'quickSort': quickSort,
+        'objectMerge': objectMerge,
+        'stringToIntArray': stringToIntArray,
+        'universalCounter': universalCounter,
+        'weedOut': weedOut,
+        'bump': bump,
+        'printObject': printObject
+    };
+
+})(App);
---
+++ homework/05/static/scripts/main.js
@@ -0,0 +1,107 @@
+( function(app) {
+    'use strict';
+
+    var task1 = function() {
+
+    };
+
+    var task2 = (a, b) => {
+        // Заданы два массива A и B необходимо их объединить
+        // в один массив C так, чтобы в массиве остались уникальные
+        // (неповторяющиеся) элементы.
+        // Например: A = [1,2], B = [2,3] получим С = [1, 2, 3].
+
+        if ( a && !b ) { return a };
+        if ( b && !a ) { return b };
+
+        if ( a instanceof Array && b instanceof Array ) {
+            return Array.from(new Set([...a, ...b])); // спёр в интернете
+        };
+
+        return [];
+    };
+
+    var task3 = (n) => {
+        // Написать отдельную функцию, которая выводит пользователю
+        // заданное число чисел Фибоначчи.
+        // (например, первых 8 чисел Фибоначчи:  0, 1, 1, 2, 3, 5, 8, 13, 21).
+        // Заданное число передается функции в качестве аргумента.
+        // Про числа Фибоначчи: https://ru.wikipedia.org/wiki/Числа_Фибоначчи
+        var fib = [1, 0],
+            i = 0;
+        while (i < n) {
+            fib[i++ % 2] = fib[0] + fib[1];
+        };
+        return fib[+!(i % 2)];
+    };
+
+    var task4 = (n) => {
+        // Напишите функцию (), которая в зависимости от переданных
+        // в нее целочисленных аргументов "количество дней", будет выводить
+        // слово "день" в нужно форме ("если передали - 1, то 1 день",
+        // "если передали - 3, то 3 дня" и т.д).
+        var n = n.toString();
+        var l = n.length;
+        if ( l > 1 && n[l - 2] === '1' ) {
+            return `${ n } дней`;
+        } else {
+            switch ( n[l - 1] ) {
+                case '0':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    return `${n} дней`;
+                    break; // Знаю, что
+                case '1':
+                    return `${n} день`;
+                    break; // не нужно брякать
+                case '2':
+                case '3':
+                case '4':
+                    return `${n} дня`;
+                    break; // ничего не могу с собой поделать.
+            };
+        };
+    };
+
+
+    document.addEventListener('DOMContentLoaded', function(){
+        var spans = document.querySelectorAll('[class^="task"] > span')
+            ;
+
+        var task2Arr = document.querySelector('.task2 input.arr')
+            , task2Brr = document.querySelector('.task2 input.brr')
+            , task2Handler = function(e){
+                    var newArr = task2(app.tools.stringToIntArray(task2Arr.value), app.tools.stringToIntArray(task2Brr.value));
+                    if ( newArr ) {
+                        spans[1].innerHTML = `[ ${newArr.join(', ')} ]`;
+                    };
+                }
+            ;
+
+        task2Arr.addEventListener('keyup', task2Handler);
+        task2Brr.addEventListener('keyup', task2Handler);
+
+    //     var game = new X0 ({
+    //         fieldSelector: '.task2 .table-wrapper table',
+    //         scoreSelector: '.task2 span',
+    //         resetButtonSelector: '.task2 button'
+    //     });
+
+
+        var task3Input = document.querySelector('.task3 input[type="text"]');
+
+        task3Input.addEventListener('keyup', function(){
+            var n = app.tools.stringToIntArray(this.value)[0];
+            spans[2].innerHTML = n ? task3(n) : '0';
+        });
+
+        var task4Input = document.querySelector('.task4 input[type="text"]')
+        task4Input.addEventListener('keyup', function(){
+            var n = app.tools.stringToIntArray(this.value)[0];
+            if ( n ) { spans[3].innerHTML = task4(n); } else { spans[3].innerHTML = ''; }
+        });
+    });
+} )(App);
