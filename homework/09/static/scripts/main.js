e01fd10428ad1bf0f270ef766fb46c0551f57eb8
51ck <sic.popper@gmail.com>
2017-06-05 17:08:00 +0300

    homework #9 2/3 fix

--- homework/09/static/scripts/main.js
+++ homework/09/static/scripts/main.js
@@ -30,27 +30,5 @@
             this.innerHTML = ( c || c === 0 ) ? ++c : 0;
         } );
 
-
-
-
-        var task3Button = document.querySelector('.task3 button');
-
-        var TrafficLight = task3();
-        var tLight = new TrafficLight('traffic-light');
-        var trigger = true;
-        task3Button.addEventListener( 'click', function() {
-            task3Button.disabled = true;
-            setTimeout( function(){
-                task3Button.disabled = false;
-            }, tLight.timeout );
-
-            tLight.trigger( trigger );
-
-            trigger = !trigger;
-        } );
-
-
-
-
     });
 } )(App);
